4d0df0ff31904daa501a207027573053aec089f9
Hendrio Marques <hendriomm@gmail.com>
2024-07-08 13:44:05 -0400

    Adding login screen, List screen

---
+++ app/src/main/java/br/edu/icomp/plaintext/ListActivity.java
@@ -0,0 +1,35 @@
+package br.edu.icomp.plaintext;
+
+import static br.edu.icomp.plaintext.R.*;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textBemVindo = findViewById(R.id.textBemVindo);
+
+        Intent intent = getIntent();
+        String login = intent.getStringExtra("login");
+
+        textBemVindo.setText("Olá " + login + "!");
+    }
+}
--- app/src/main/java/br/edu/icomp/plaintext/MainActivity.java
+++ app/src/main/java/br/edu/icomp/plaintext/MainActivity.java
@@ -1,6 +1,11 @@
 package br.edu.icomp.plaintext;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void entrarClicado (View view) {
+//        EditText editText = findViewById(R.id.editLogin);
+//        String login = editText.getText().toString();
+//        String msg = "Olá " + login + "!!";
+//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ListActivity.class);
+        EditText inputLogin = findViewById(R.id.editLogin);
+        intent.putExtra("login", inputLogin.getText().toString());
+        startActivity(intent);
     }
 }
